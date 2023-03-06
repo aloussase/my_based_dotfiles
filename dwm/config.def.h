783ba81a31bebdf2ab6934d118be79b120a3ed06
Alexander Goussas <goussasalexander@gmail.com>
2023-03-06 15:24:21 -0500

    dwm: Update colorscheme

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -11,15 +11,10 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Hasklug NFM:size=10" };
 
 /* Colorschem */
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
   /*               fg         bg         border   */
-  [SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-  [SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+  [SchemeNorm] = { "#444444", "#000000", "#ff0000" },
+  [SchemeSel]  = { "#ff0000", "#000000", "#ff0000" },
 };
 
 /* Workspaces */
